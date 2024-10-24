6c22cece60cf35ef1cf023cd5d9f9e26d4d2e27f
Paul Glavich <paulglavich@microsoft.ccom>
2024-10-25 10:49:43 +1100

    added some nicer styling, saving of details

--- src/content.js
+++ src/content.js
@@ -11,91 +11,104 @@
   //extractLicenseTerms();
   console.log('adding message listener');
 
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.aoaiDetails) {
-      console.log('Received AOAI Details');
-      // Handle the received AOAI details here
-      // For example, you can call a function to process the details
-      processAoaiDetails(message.aoaiDetails);
+function fireDomainEvent(eventName) {
+  chrome.runtime.sendMessage({ action: eventName }, response => {
+    if (chrome.runtime.lastError) {
+      console.error("Error sending message:", chrome.runtime.lastError);
+    } else {
+      console.log("Message sent to popup.js:", response);
     }
   });
-  
-  function processAoaiDetails(aoaiDetails) {
-    // Implement your logic to handle the AOAI details here
-    console.log('Processing AOAI Details');
-    const context = extractLicenseTerms();
-    console.log('Terms:', context);
+}
 
-    const response = callOpenAI(aoaiDetails.aoaiEndpoint, aoaiDetails.deploymentName, aoaiDetails.apiVersion, aoaiDetails.apiKey,context);
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.aoaiDetails) {
+    console.log('Received AOAI Details');
+    // Handle the received AOAI details here
+    // For example, you can call a function to process the details
+    processAoaiDetails(message.aoaiDetails);
   }
+});
 
-  console.log('in content.js');
+function processAoaiDetails(aoaiDetails) {
+  // Implement your logic to handle the AOAI details here
+  console.log('Processing AOAI Details');
+  fireDomainEvent('extractLicenseTerms');
+  const context = extractLicenseTerms();
+  //console.log('Terms:', context);
 
-  function extractLicenseTerms() {
-    console.log('in extractLicenseTerms');
-    const terms = document.body.innerText.match(/(terms of service|license agreement|user agreement|privacy policy|terms and conditions)/i);
-    //console.log(document.body.innerText);
-    if (terms) {
-      //alert("License Terms Found: " + terms[0]);
-      console.log("License Terms Found: " + terms[0]);
-      return document.body.innerText;
-      
-    } else {
-      alert("No license terms found on this page.");
-    }
-  }
+  const response = callOpenAI(aoaiDetails.aoaiEndpoint, aoaiDetails.deploymentName, aoaiDetails.apiVersion, aoaiDetails.apiKey,context);
+}
 
-  function callOpenAI(aoaiEndpoint, deploymentName, apiVersion, apiKey, context) {
-    //"https://aoai-evaltest.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2024-02-15-preview"
-    const endpoint = `${aoaiEndpoint}/openai/deployments/${deploymentName}/chat/completions?api-version=${apiVersion}`;
-    const headers = {
-      "Content-Type": "application/json",
-      "api-key": apiKey,
-    };
-    const payload = {
-      "messages": [
-        {
-          "role": "system",
-          "content": [
-            {
-              "type": "text",
-              "text": "You are an AI assistant that helps summarises information and highlights privacy concerns where personal data might sold, shared or distributed."
-            }
-          ]
-        },
-        {
-          "role": "user",
-          "content": [
-            {
-              "type": "text",
-              "text": "Can you summarise the privacy policy of this website provided in the context as succinctly as possible?" + 
-                      "If there is any mention or potential risk of personal data being sold, shared or distributed,"+
-                      "please highlight it and ensure this information is noted at the top of your summary. Context: " + context
-            }
-          ]
-        }
-      ],
-      "temperature": 0.7,
-      "top_p": 0.95,
-      "max_tokens": 800
-    }
+  //console.log('in content.js');
 
-    console.log('Calling OpenAI API:', endpoint);
-
-    fetch(endpoint, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(payload)
-    })
-    .then(response => response.json())
-    .then(data => {
-      //console.log('OpenAI API response:', data);
-      console.log("** SUMMARY **");
-      console.log(data.choices[0].message.content);
-  
-    })
-    .catch(error => {
-      console.error('Error calling OpenAI API:', error);
-    });
+function extractLicenseTerms() {
+  console.log('in extractLicenseTerms');
+  const terms = document.body.innerText.match(/(terms of service|license agreement|user agreement|privacy policy|terms and conditions)/i);
+  //console.log(document.body.innerText);
+  if (terms) {
+    //alert("License Terms Found: " + terms[0]);
+    console.log("License Terms Found");
+    return document.body.innerText;
     
+  } else {
+    alert("No license terms found on this page.");
+  }
+}
+
+function callOpenAI(aoaiEndpoint, deploymentName, apiVersion, apiKey, context) {
+  //"https://aoai-evaltest.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2024-02-15-preview"
+  const endpoint = `${aoaiEndpoint}/openai/deployments/${deploymentName}/chat/completions?api-version=${apiVersion}`;
+  const headers = {
+    "Content-Type": "application/json",
+    "api-key": apiKey,
   };
+  const payload = {
+    "messages": [
+      {
+        "role": "system",
+        "content": [
+          {
+            "type": "text",
+            "text": "You are an AI assistant that helps summarises information and highlights privacy concerns where personal data might sold, shared or distributed."
+          }
+        ]
+      },
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": "Can you summarise the privacy policy of this website provided in the context as succinctly as possible?" + 
+                    "If there is any mention or potential risk of personal data being sold, shared or distributed,"+
+                    "please highlight it and ensure this information is noted at the top of your summary. Context: " + context
+          }
+        ]
+      }
+    ],
+    "temperature": 0.7,
+    "top_p": 0.95,
+    "max_tokens": 800
+  }
+
+  console.log('Calling OpenAI API:', endpoint);
+
+  fetch(endpoint, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(payload)
+  })
+  .then(response => response.json())
+  .then(data => {
+    //console.log('OpenAI API response:', data);
+    fireDomainEvent('end-extractLicenseTerms');
+    alert(data.choices[0].message.content);
+    // console.log("** SUMMARY **");
+    // console.log(data.choices[0].message.content);
+
+  })
+  .catch(error => {
+    console.error('Error calling OpenAI API:', error);
+  });
+  
+};
--- src/popup.js
+++ src/popup.js
@@ -1,31 +1,64 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'extractLicenseTerms') {
+    document.getElementById('content').style.display = 'none';
+    document.getElementById('spinner').style.display = 'block';
+    console.log('Message received to extract license terms');
+  }
+  if (request.action === 'end-extractLicenseTerms') {
+    document.getElementById('content').style.display = 'block';
+    document.getElementById('spinner').style.display = 'none';
+    console.log('Message received to extract license terms');
+  }
+  sendResponse('event message: [' + request.action + '] received');
+//  return true;
 
-
-document.getElementById('extract').addEventListener('click', () => {
-  console.log('hooking up click event');
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    console.log('hooking event to tab');
-    const oaiDetails = getAoaiDetails();
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-//      function: extractLicenseTerms
-    }, () => {
-      chrome.tabs.sendMessage(tabs[0].id, { aoaiDetails: oaiDetails  });
-    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+  chrome.storage.local.get(['oaiDetails'], (result) => {
+    if (result.oaiDetails) {
+      document.getElementById('aoaiEndpoint').value = result.oaiDetails.aoaiEndpoint;
+      document.getElementById('deploymentName').value = result.oaiDetails.deploymentName;
+      document.getElementById('apiVersion').value = result.oaiDetails.apiVersion;
+      document.getElementById('apiKey').value = result.oaiDetails.apiKey;
+      console.log('OAI details loaded');
+    }
   });
-})
+
+  document.getElementById('extract').addEventListener('click', () => {
+    console.log('hooking up click event');
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      console.log('hooking event to tab');
+      const oaiDetails = getAoaiDetails();
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ['popup.js']
+  //      function: extractLicenseTerms
+      }, () => {
+        chrome.tabs.sendMessage(tabs[0].id, { aoaiDetails: oaiDetails  });
+      });
+    });
+  })
+  
+});
+
 
 function getAoaiDetails() {
   const aoaiEndpoint = document.getElementById('aoaiEndpoint');
   const deploymentName = document.getElementById('deploymentName');
   const apiVersion = document.getElementById('apiVersion');
   const apiKey = document.getElementById('apiKey');
-
-  return {
+  const oaiDetails = {
     aoaiEndpoint: aoaiEndpoint.value,
     deploymentName: deploymentName.value,
     apiVersion: apiVersion.value,
     apiKey: apiKey.value
   }
+
+  chrome.storage.local.set({ oaiDetails: oaiDetails }, () => {
+    console.log('OAI details saved');
+  });
+
+  return oaiDetails;
 }
   
   
